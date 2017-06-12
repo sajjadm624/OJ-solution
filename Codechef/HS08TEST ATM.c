f64c6827598a586fb37a373226cfe618fe72e93b
Sajjad Hossain <sajjadm624@gmail.com>
2017-06-13 04:21:42 +0600

    Add files via upload

---
+++ Codechef/HS08TEST ATM.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    int x;
+    float y;
+    scanf("%d %f", &x, &y);
+   if(x+0.5<y)
+    {
+       if(x%5==0)
+        {
+        printf("%.2f", y-x-0.5);
+        }
+        else
+           printf("%.2f",y);
+    }
+    else
+        printf("%.2f",y);
+
+    return 0;
+}
