cb4fadabefb30fd099656bb81f81efbbe27b6792
Sajjad Hossain <sajjadm624@gmail.com>
2017-07-24 02:02:14 +0600

    Add files via upload

--- URI/uri 1186.c
+++ URI/uri 1186.c
@@ -1,16 +1,38 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j;
-    scanf("%d", &n);
-    for(i=0; i<n; i++){
-        for(j=0;j<(n*2)-1;j++){
-                if(j%2==0)
-            printf("1", n);
-                else
-                    printf(" ");
 
+    double m[12][12], avg=0, sum=0;
+    int i, j, p=11;
+    char O;
+
+    scanf("%c", &O);
+
+    for(i=0 ; i<12 ; i++)
+    {
+        for(j=0 ; j<12 ; j++)
+        {
+            scanf("%lf", &m[i][j]);
         }
-        printf("\n");
     }
+
+    for(i=1 ; i<=11 ; i++)
+    {
+        for(j=11; j>=p ; j--)
+        {
+            sum=sum + m[i][j];
+        }
+        p--;
+    }
+
+    if(O == 'S')
+        printf("%0.1lf\n", sum);
+
+    else if (O == 'M')
+    {
+        avg = sum / 66.0;
+        printf("%0.1lf\n", avg);
+    }
+
+    return 0;
 }
